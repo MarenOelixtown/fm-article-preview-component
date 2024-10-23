7579f8ad2d8df9d76c02d59b4398a4fdb94c13b7
Maren <marenehlers@gmx.net>
2024-10-23 17:53:32 +0200

    adjusting shareButton-function and styling share-popup for desktop

--- js/index.js
+++ js/index.js
@@ -4,10 +4,22 @@ const sharePopup = document.querySelector('[data-js="share-popup"]');
 const shareButton = document.querySelector('[data-js="share-button"]');
 const buttonIcon = document.querySelector('[data-js="button-icon"]');
 
+const breakpoint = 1200;
+
 shareButton.addEventListener("click", (event) => {
-  footer.classList.toggle("show-popup--bg-color");
-  author.classList.toggle("show-popup");
-  sharePopup.classList.toggle("show-popup");
-  shareButton.classList.toggle("show-popup__button--dark");
-  buttonIcon.classList.toggle("show-popup-button__icon--white");
+  if (window.innerWidth < breakpoint) {
+    footer.classList.toggle("show-popup--bg-color");
+    author.classList.toggle("show-popup");
+    sharePopup.classList.toggle("show-popup");
+    shareButton.classList.toggle("show-popup__button--dark");
+    buttonIcon.classList.toggle("show-popup-button__icon--white");
+  } else {
+    sharePopup.classList.toggle("show-popup");
+    shareButton.classList.toggle("show-popup__button--dark");
+    buttonIcon.classList.toggle("show-popup-button__icon--white");
+  }
+});
+
+window.addEventListener("resize", () => {
+  window.location.reload();
 });
